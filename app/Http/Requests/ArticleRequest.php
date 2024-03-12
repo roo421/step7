ca54ff60ef48b243558695873246e1f31b4fe335
ROO <konp6858@gmail.com>
2024-03-12 22:01:32 +0900

    step7のやつ

---
+++ app/Http/Requests/ArticleRequest.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ArticleRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            //
+        ];
+    }
+
+    /**
+ * 項目名
+ *
+ * @return array
+ */
+public function attributes()
+{
+    return [
+        'title' => 'タイトル',
+        'url' => 'URL',
+        'comment' => 'コメント',
+    ];
+}
+
+/**
+ * エラーメッセージ
+ *
+ * @return array
+ */
+public function messages() {
+    return [
+        'title.required' => ':attributeは必須項目です。',
+        'title.max' => ':attributeは:max字以内で入力してください。',
+        'url.required' => ':attributeは必須項目です。',
+        'url.max' => ':attributeは:max字以内で入力してください。',
+        'url.url' => ':attributeはURL形式で入力してください。',
+        'comment.max' => ':attributeは:max字以内で入力してください。',
+    ];
+}
+}
---
+++ app/Models/Article.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+
+class Article extends Model
+{
+    use HasFactory;
+    
+    public function getList() {
+        // articlesテーブルからデータを取得
+        $articles = DB::table('articles')->get();
+
+        return $articles;
+    }
+}
